35938baa16135aa887eee811d81aba04f53c0c9e
Zachary Lewis <devcmontgomery@gmail.com>
2019-12-24 11:33:59 -0500

    k

---
+++ lesson01/app.js
@@ -0,0 +1,17 @@
+var basePath = __dirname;
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+ 
+  filename = './indexchange.html';  
+  if (req.url == "/")
+     filename = "index.html";
+
+  console.log(req.url);
+  fs.readFile(filename, function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  });
+}).listen(8080);
---
+++ lesson01/project01.py
@@ -0,0 +1,18 @@
+# Python First Program
+# Demo some key basics
+#
+# Assignment: Change myname to your name and change the numofchildren to how many children you have.
+#
+
+myname = "Zach Lewis more"
+
+print("My Name is: " + myname);
+
+numofchildren = 1 + 1 + 1;
+
+#print("I have " + numofchildren + " Children.");
+
+print("I have " + str(numofchildren) + " Children.");
+
+
+
